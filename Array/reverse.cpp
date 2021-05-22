67db87421ed8dced8cb1f3a57487a2c1363bf444
Abhishek01Thakur <abbuin22@gmail.com>
2021-05-23 02:07:25 +0530

    reverse array

--- Array/reverse.cpp
+++ Array/reverse.cpp
@@ -1,8 +1,40 @@
-#include<stdio.h>
-#include<conio.h>
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
 
 int main(){
-	printf("hello");
+	int a[]={2,3,4,5,6};
+	int b[]={5,9,1,0};
+	int i,j,temp;
+	
+	i=0;
+	j=4;
+	while(i<j)   //odd size array
+	{
+		temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+		i++;
+		j--;
+	}
+	i=0;
+	j=3;
+	while(i<j) //even size array
+	{
+		temp=b[i];
+		b[i]=b[j];
+		b[j]=temp;
+		i++;
+		j--;
+	}
+	for(i=0;i<5;i++)
+	cout<<a[i]<<" ";
+	cout<<endl;
+	for(i=0;i<4;i++)
+	cout<<b[i]<<" ";
 		
 	return 0;
 }
