ae5dae9ed6c2ab4481871967700e756cb0432af0
Abhishek01Thakur <69719886+Abhishek01Thakur@users.noreply.github.com>
2022-02-18 20:09:42 +0530

    Create Diameter_Tree.cpp

---
+++ Tree/Diameter_Tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int height(TreeNode *root){
+        
+        if(root==NULL)
+            return 0;
+        
+        int left = height(root->left);
+        int right = height(root->right);
+        
+        return max(left,right)+1;
+        
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+        if(root==NULL)
+            return 0;
+        
+        int op1=diameterOfBinaryTree(root->left);
+        int op2=diameterOfBinaryTree(root->right);
+        int op3=height(root->left)+ height(root->right);
+        
+        return max(op1,max(op2,op3));
+        
+    }
+};
