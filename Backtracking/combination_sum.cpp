418f0e0bd6cee20e4b264887913484b11d151528
Abhishek01Thakur <69719886+Abhishek01Thakur@users.noreply.github.com>
2022-02-24 19:18:40 +0530

    Create combination_sum.cpp

---
+++ Backtracking/combination_sum.cpp
@@ -0,0 +1,61 @@
+/*Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. 
+You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+
+*/
+
+
+class Solution {
+public:
+    void solve(int idx,int n,vector<int> v,vector<vector<int>> &ans,vector<int> c,int tar,int sum)
+    {   
+        if(sum==tar){
+            ans.push_back(v);
+            return;
+        }
+        if(sum>tar) return;
+        
+        
+        for(int i=idx;i<n;i++){     // to remove duplicate answers i.e [2,2,3] and [3,2,2] we will not look behind in the array and start from the current index only
+            v.push_back(c[i]);
+            solve(i,n,v,ans,c,tar,sum+c[i]);
+            v.pop_back();
+            
+        }
+        
+    }
+    
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<int> v;
+        vector<vector<int>> ans;
+        int n=candidates.size();
+        solve(0,n,v,ans,candidates,target,0);
+        
+        return ans;
+        
+    }
+};
