9ec55f2ff9032bcc4a97cf9dcd29e0036d5ed1c6
Abhishek01Thakur <69719886+Abhishek01Thakur@users.noreply.github.com>
2022-02-25 23:19:13 +0530

    Create max_sum_non_adjacent_nodes.cpp

---
+++ Tree/max_sum_non_adjacent_nodes.cpp
@@ -0,0 +1,43 @@
+/*
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+
+class Solution{
+  public:
+    //Function to return the maximum sum of non-adjacent nodes.
+    
+    pair<int,int> solve(Node *root)
+    {
+        if(root==NULL)
+        {
+            pair<int,int> p = {0,0};
+            return p;
+        }
+        
+        
+        pair<int,int> left = solve(root->left);
+        pair<int,int> right = solve(root->right);
+        
+        pair<int,int> res;
+        
+        res.first = root->data + left.second + right.second;
+        
+        res.second = max(left.first,left.second) + max(right.first,right.second);
+        
+        return res;
+        
+    }
+    
+    
+    int getMaxSum(Node *root) 
+    {
+        // Add your code here
+        pair<int,int> ans=solve(root);
+        return max(ans.first,ans.second);
+    }
+};
