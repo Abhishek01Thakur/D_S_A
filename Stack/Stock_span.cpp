810907f883fece809caa9fbf1fa7693f6c32deb4
Abhishek01Thakur <69719886+Abhishek01Thakur@users.noreply.github.com>
2022-08-17 10:55:49 +0530

    Create Stock_span.cpp

---
+++ Stack/Stock_span.cpp
@@ -0,0 +1,27 @@
+
+ int* span(int arr[],int n){
+  
+     int res[n];
+     stack<int> s;
+
+     res[0]=1;
+     s.push(0);   //pushed indices in the stack
+
+     for(int i=1; i < n ; i++){
+
+         while(!s.empty() && arr[i] > arr[st.top()]){
+              s.pop();
+         }
+
+         if(s.empty()) res[i] = i+1;
+         else{
+            res[i] = i -  s.top();
+         }
+
+         s.push(i);
+     }
+      int *p=res;
+
+      return p;
+  
+}
